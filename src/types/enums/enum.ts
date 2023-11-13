e1074c39fc8f8ad85bb939b04a1c1ba68da464f4
kamyar kamali <kamyarkamali021@gmail.com>
2023-11-14 00:32:32 +0330

    Add functionatily enum and interface typeScript in data

--- src/types/enums/enum.ts
+++ src/types/enums/enum.ts
@@ -1,5 +1,9 @@
 enum DATAS{
     CATEGORY_BIG="بزرگسالان",
     CATEGORY_CHILDREN="کودکان",
-    
+    CATEGORY_TARIKHI="تاریخی",
+    CATEGORY_ELMI="علمی",
+    CATEGORY_ROMANS="رمان"
 }
+
+export {DATAS};
